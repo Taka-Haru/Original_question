19bc6c365d1148e202762f54b44aec1cd8456c65
TakaHaru <xt.sakamoto@gmail.com>
2015-12-26 20:02:28 +0900

    ４問のオリジナル問題提出

---
+++ question2.php
@@ -0,0 +1,7 @@
+<?php
+    for($i=31;$i<=50;$i++){
+      if($i%2 == 0){
+      continue;
+      }
+      echo $i.'<br>';
+    }
---
+++ question3.php
@@ -0,0 +1,11 @@
+<?php
+	$arr = array(
+            1 => 'AAA',
+      'hello' => 'world',
+      'soeda' => 30,
+           20 => 20
+    );
+	foreach($arr as $key => $value){
+		echo $key.'は'.$value.'です。';
+		echo '<br>';
+    }
