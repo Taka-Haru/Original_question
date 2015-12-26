6a17f807d104f44c7fc228954b487e359642f63c
TakaHaru <xt.sakamoto@gmail.com>
2015-12-26 20:33:52 +0900

    問題3

--- question3.php
+++ question3.php
@@ -1,11 +1,11 @@
 <?php
 		$arr = array(
-	            1 => 'AAA',
-	      'hello' => 'world',
-	      'soeda' => 30,
-	           20 => 20
+							1 => 'AAA',
+				'hello' => 'world',
+				'soeda' => 30,
+						 20 => 20
 		);
 		foreach($arr as $key => $value){
 			echo $key.'は'.$value.'です。';
 			echo '<br>';
-	  }
+		}
